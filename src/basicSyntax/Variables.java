67cee6752135464a1a39beb55b9d5be91978fd27
ゲスト <@PC188>
2024-03-04 13:25:35 +0900

    variablesの実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,17 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1 = 7;
+		double num2 = 3.14;
+		System.out.println(num1);
+		System.out.println(num2);
+
+		String myFavorite = "私の好きな";
+		
+		System.out.println(myFavorite + "整数は" + num1 + "です。");
+		System.out.println(myFavorite + "小数は" + num2 + "です。");
+	}
+
+}
